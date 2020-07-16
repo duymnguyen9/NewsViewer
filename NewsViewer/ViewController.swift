551887c67f095690fbc0098888e64620ad72ba4d
Duy Nguyen <duy.m.nguyen9@gmail.com>
2020-07-16 12:52:46 -0700

    Initial Commit

---
+++ NewsViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsViewer
+//
+//  Created by Duy Nguyen on 7/16/20.
+//  Copyright © 2020 Duy Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
